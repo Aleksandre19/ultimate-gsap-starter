37628c9aabeda4370e263e24cb876fd623dab071
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2025-08-05 18:34:17 +0200

    01a: Additional task - adding glowing effect

--- pages/01a_your-first-animation/script.js
+++ pages/01a_your-first-animation/script.js
@@ -1,13 +1,36 @@
+// gsap.to(".card", {
+//     opacity: 1,
+//     scale: 1,
+//     duration: 5,
+//     onComplete: () => {
+//         gsap.to(".card", {
+//             y: -20,
+//             repeat: -1,
+//             yoyo: true,
+//             duration: 1,
+//         });
+//     }
+// });
+
+// Additional exercise
 gsap.to(".card", {
-    opacity: 1,
-    scale: 1,
-    duration: 5,
-    onComplete: () => {
-        gsap.to(".card", {
-            y: -20,
-            repeat: -1,
-            yoyo: true,
-            duration: 1,
-        });
-    }
+  opacity: 1,
+  scale: 1,
+  duration: 5,
+  onComplete: () => {
+    gsap.to(".card", {
+      y: -20,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+    });
+
+    // Pulsing glow animation
+    gsap.to(".card", {
+      boxShadow: "0 20px 80px rgba(29, 209, 161, 0.6)",
+      repeat: -1,
+      yoyo: true,
+      duration: 1,
+    });
+  },
 });
